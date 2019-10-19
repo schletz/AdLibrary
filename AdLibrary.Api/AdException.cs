fc77e62b0dd26a08519f9bbfc55a3e2edc2b0b09
Michael Schletz <schletz@spengergasse.at>
2019-10-19 07:20:50 +0200

    Commit 20191019

---
+++ AdLibrary.Api/AdException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdLibrary.Api
+{
+    public class AdException : Exception
+    {
+        public AdException() { }
+        public AdException(string message) : base(message) { }
+        public AdException(string message, Exception inner) : base(message, inner) { }
+    }
+}
